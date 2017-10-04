3f75c8241a8a0fc605e5678be00708e642b823ca
Zack Stout <zackstout@gmail.com>
2017-10-04 08:11:29 -0500

    it has been done

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
-
+function State(state, rev, count){
+  this.state = state;
+  this.revenue = rev;
+  this.locationCount = count;
+  this.averageRevenue = function(){
+    return Math.round(this.revenue / this.locationCount);
+  };
+}
 // object instances
+var Ill = new State("Illinois", 5000, 12);
+var Minn = new State("Minnesota", 300, 3);
+var Nev = new State("Nevada", 25000, 1);
 
 // push object instances to locations array
+locations.push(Ill);
+locations.push(Minn);
+locations.push(Nev);
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
